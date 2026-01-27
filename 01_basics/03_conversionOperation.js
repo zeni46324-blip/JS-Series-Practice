b7c5a06312a6639b9bdcbf66249260b0528525b7
zeni46324-blip <zeni46324@gmail.com>
2026-01-27 18:44:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+                   //practice 1
+// let score = 33
+// console.log (typeof score);
+//  console.log (typeof (score));
+
+
+                   /*practice 2 ----is main hum ny 33 ko string bna k dekha k
+                    type kya show krwata hy */
+//  let score ="33"
+//  console.log (typeof score);
+//  console.log (typeof (score));
+
+          /* practice 3 -------ab is main dekhain gy jo 33 ko as a string liya tha
+                ab conversion k baat type number show krwata hy yan string hi */
+//let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+
+               // practice 4 -------show NaN mean not a number
+// let score ="33abc"
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+//let score = null
+// let score = undefined
+// let score = true
+
+            //information
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+// false => 0
+// "" => false
+// "umama" => true
+
+          //practice --- hum chahty hain true likha aye but variable value 1 hy
+ let isLoggedIn =1  
+ let booleanisLoggedIn = Boolean(isLoggedIn) 
+ console.log (booleanisLoggedIn)
