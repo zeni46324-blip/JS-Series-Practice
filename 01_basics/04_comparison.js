96f8f92bcc80e30e31ba21364c71bafdcb56f68f
zeni46324-blip <zeni46324@gmail.com>
2026-01-28 19:48:13 +0000

    do comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+                 //---------- simple 
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+           //comparison k liye conversion----string k number main convert kr diya
+
+// console.log("2"> 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefine>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+               //      === (triple type)
+console.log("2"===2);
